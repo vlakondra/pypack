78a12224eb5d90201e3990b75bef680933354d32
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-04-06 02:36:32 +0000

    change import in start

--- start.py
+++ start.py
@@ -1,7 +1,14 @@
 import os, sys
+sr= os.path.join(os.getcwd() ,'src')
+print('sr',sr)
+sys.path.insert(0, sr)
+
 from src.newlib import lib
+from newlib import lib as lb
 
 # import src.newlib.lib
 
 lib.print_table()
 print(sys.path)
+
+lb.print_table()
